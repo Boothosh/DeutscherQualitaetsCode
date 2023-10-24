dcbb7b4f5108588c54b0a632fc2cc735e2b5c03f
MaxEckstein <79403576+Boothosh@users.noreply.github.com>
2023-10-24 22:59:37 +0200

    Falschen Kommentar gelöscht und Variablen umbenannt. Außerdem unnötiges "hoehenIndex = 0" gelöscht. Weglänge und die Punkte über die der Weg geführt hat werden nun am Ende des Programmes ausgegeben. Problem behoben, dass der hoehenIndex beim einlesen nicht zwischen den Stockwerken zurückgesetzt wurde, und somit die x2-Koordinate der Punkte im zweiten Stockwerk falsch war.

--- Aufgabe 3/main.swift
+++ Aufgabe 3/main.swift
@@ -17,55 +17,54 @@ guard let text: String = try? String(contentsOf: pfad) else {
     exit(-1)
 }
 var zeilen = text.split(whereSeparator: \.isNewline)
-guard let stockwerkLaengeAlsString = zeilen.removeFirst().split(separator: " ").first, let langeEinsesStockwerks = Int(stockwerkLaengeAlsString) else {
-    print("Länge eines Stockwerks konnte nicht ermittelt werden.")
+guard let hoeheEinesStockwerksString = zeilen.removeFirst().split(separator: " ").first, let hoeheEinesStockwerks = Int(hoeheEinesStockwerksString) else {
+    print("Höhe eines Stockwerks konnte nicht ermittelt werden.")
     exit(-1)
 }
 
-let stockwerke: [[Character]] = zeilen.map { zeile in
+let stockwerkZeilen: [[Character]] = zeilen.map { zeile in
     return zeile.map({$0})
 }
 
-// X, Y, Z- Koordinate (in dieser Reihenfolge)
 var start: Punkt?
 var ziel: Punkt?
 
 var hoehenIndex = 0
 
-for zeile in stockwerke {
+for zeile in stockwerkZeilen {
     for char in zeile.enumerated() {
+        let x1 = char.offset
+        let x2 = (hoehenIndex < hoeheEinesStockwerks) ? hoehenIndex : (hoehenIndex - hoeheEinesStockwerks)
+        let x3 = (hoehenIndex < hoeheEinesStockwerks) ? 0 : 1
         if char.element == "A" {
-            start = Punkt(x1: char.offset, x2: hoehenIndex, x3: hoehenIndex < langeEinsesStockwerks ? 0 : 1)
-        }
-        if char.element == "B" {
-            ziel = Punkt(x1: char.offset, x2: hoehenIndex, x3: hoehenIndex < langeEinsesStockwerks ? 0 : 1)
+            start = Punkt(x1: x1, x2: x2, x3: x3)
+        } else if char.element == "B" {
+            ziel = Punkt(x1: x1, x2: x2, x3: x3)
         }
     }
     hoehenIndex += 1
 }
-
-hoehenIndex = 0
 
 guard let start, let ziel else {
     print("Es konnte kein Start- und/oder Endpunkt gefunden werden.")
     exit(-1)
 }
 
-// MARK: Algorithmus
+// MARK: A* Algorithmus
 
 // Heuristische Funktion (schätzt noch zu brauchende Zeit)
 func h(_ punkt: Punkt) -> Int {
     return abs(ziel.x1 - punkt.x1) + abs(ziel.x2 - punkt.x2) + abs(ziel.x3 - punkt.x3) * 3
 }
 
-func reconstruct_path(_ cameFrom: [Punkt: Punkt], _ current: Punkt) -> [Punkt] {
-    var total_path = [current]
-    var aktuellerPunkt = current
-    while let neuerPunkt = cameFrom[aktuellerPunkt] {
+func rekonstruiereWeg(_ kommtVon: [Punkt: Punkt], _ endPunkt: Punkt) -> [Punkt] {
+    var pfad = [endPunkt]
+    var aktuellerPunkt = endPunkt
+    while let neuerPunkt = kommtVon[aktuellerPunkt] {
         aktuellerPunkt = neuerPunkt
-        total_path.insert(neuerPunkt, at: 0)
+        pfad.insert(neuerPunkt, at: 0)
     }
-    return total_path
+    return pfad
 }
 
 func sucheNachVerbindungen(fuerPunkt punkt: Punkt) -> [(Punkt, Int)] {
@@ -81,45 +80,41 @@ func sucheNachVerbindungen(fuerPunkt punkt: Punkt) -> [(Punkt, Int)] {
         let neueX1 = punkt.x1 + i.0
         let neueX2 = punkt.x2 + i.1
         let neueX3 = punkt.x3 + i.2
-        if stockwerke[neueX2 + neueX3*langeEinsesStockwerks][neueX1] != "#" {
+        if stockwerkZeilen[neueX2 + neueX3*hoeheEinesStockwerks][neueX1] != "#" {
             let neuerPunkt = Punkt(x1: neueX1, x2: neueX2, x3: neueX3)
-            gefundeneVerbindungen.append((neuerPunkt, i.2 != 0 ? 1 : 3))
+            gefundeneVerbindungen.append((neuerPunkt, (i.2 != 0) ? 3 : 1))
         }
     }
     return gefundeneVerbindungen
 }
 
-func aStarPfad(von start: Punkt, zum ziel: Punkt) -> [Punkt]? {
+func aStarPfad(von start: Punkt, zum ziel: Punkt) -> ([Punkt], Int)? {
 
-    var openSet = [start]
+    var zuPruefendeKnoten = [start]
 
-    var cameFrom = [Punkt: Punkt]()
+    var kommtVon = [Punkt: Punkt]()
 
-    var gScore = [start: 0]
+    var weglaengeZuPunkten = [start: 0]
 
-    var fScore = [start: h(start)]
+    var zuErwartendeDauerVonPunkten = [start: h(start)]
 
-    while !openSet.isEmpty {
-        // This operation can occur in O(Log(N)) time if openSet is a min-heap or a priority queue
-        guard let current = openSet.sorted(by: {fScore[$0]! < fScore[$1]!}).first else { return nil }
+    while !zuPruefendeKnoten.isEmpty {
+        guard let current = zuPruefendeKnoten.sorted(by: {zuErwartendeDauerVonPunkten[$0]! < zuErwartendeDauerVonPunkten[$1]!}).first else { return nil }
         if current == ziel {
-            return reconstruct_path(cameFrom, current)
+            return (rekonstruiereWeg(kommtVon, current), weglaengeZuPunkten[current]!)
         }
             
-        guard let index = openSet.firstIndex(of: current) else { continue }
-        openSet.remove(at: index)
+        guard let index = zuPruefendeKnoten.firstIndex(of: current) else { continue }
+        zuPruefendeKnoten.remove(at: index)
         
         for bindung in sucheNachVerbindungen(fuerPunkt: current) {
-            // d(current,neighbor) is the weight of the edge from current to neighbor
-            // tentative_gScore is the distance from start to the neighbor through current
-            let tentative_gScore = gScore[current]! + bindung.1
-            if tentative_gScore < gScore[bindung.0] ?? 9999999999999999 {
-                // This path to neighbor is better than any previous one. Record it!
-                cameFrom[bindung.0] = current
-                gScore[bindung.0] = tentative_gScore
-                fScore[bindung.0] = tentative_gScore + h(bindung.0)
-                if !openSet.contains(bindung.0) {
-                    openSet.append(bindung.0)
+            let weglaengeZuPunkt = weglaengeZuPunkten[current]! + bindung.1
+            if weglaengeZuPunkt < weglaengeZuPunkten[bindung.0] ?? 9999999999999999 {
+                kommtVon[bindung.0] = current
+                weglaengeZuPunkten[bindung.0] = weglaengeZuPunkt
+                zuErwartendeDauerVonPunkten[bindung.0] = weglaengeZuPunkt + h(bindung.0)
+                if !zuPruefendeKnoten.contains(bindung.0) {
+                    zuPruefendeKnoten.append(bindung.0)
                 }
             }
         }
@@ -128,4 +123,14 @@ func aStarPfad(von start: Punkt, zum ziel: Punkt) -> [Punkt]? {
     return nil
 }
 
-let weg = aStarPfad(von: start, zum: ziel)
+guard let weg = aStarPfad(von: start, zum: ziel) else {
+    print("Es wurde kein Weg von A nach B gefunden.")
+    exit(-1)
+}
+
+print("\nBerechnung des Weges erfolgreich!")
+print("Der Kürzeste Weg geht über folgende Punkte:")
+for i in weg.0 {
+    print("Punkt(\(i.x1 + 1)|\(i.x2 + 1)) auf dem \(i.x3 == 0 ? "ersten" : "zweiten") Stockwerk")
+}
+print("Der Weg ist \(weg.1) Sekunden lang.")
